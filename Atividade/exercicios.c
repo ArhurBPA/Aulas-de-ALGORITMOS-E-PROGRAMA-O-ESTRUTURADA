57f09046390d43b7493d1724b940c797e2c45051
ArhurBPA <arthurbpa2005@gmail.com>
2023-08-16 10:23:02 -0300

    Atividade1

--- Atividade/exercicios.c
+++ Atividade/exercicios.c
@@ -1,36 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
 
-main(){
-    
+int main(){
+
     char nome[30];
-    char idade[10];
-    char matricula[17];
-    char endereco[30];
-    char curso[17];
-    char periodo[20];
-    char diciplinas[100];
+    int idade;
+    char matricula[30];
+    char endereco[50];
+    char curso[20];
+    int periodo;
+    char disciplinas[60];
 
-    printf("Digite seu nome e sua idade: \n");
-    fgets(nome, 30, stdin);
-    fgets(idade, 10, __stdinp);
+    printf("_________________\n");
+    printf("Cadastro do Aluno\n");
+    printf("_________________\n\n");
+    printf("Fale o seu nome: ");
+    fgets(nome,30,stdin);
 
-    printf("Agora sua matricula:\n");
-    fgets(matricula, 17, stdin);
+    printf("Fale sua idade: ");
+    scanf("%d", &idade);  
+    
     fflush(stdin);
-
-    printf("Digite o seu endereco:\n");
-    fgets(endereco, 30, stdin);
-
-    printf("Qual curso voce esta fazendo e em qual peiodo vc esta?\n");
-    fgets(curso, 17, stdin);
+    printf("Digite sua matricula:\n ");
+    fgets(matricula,30,stdin);
     fflush(stdin);
-    fgets(periodo, 20, stdin);
+    printf("Digite seu endereco:\n ");
+    fgets(endereco,50,stdin);
     fflush(stdin);
-
-    printf("Quais diciplinas voce possue?\n");
-    fgets(diciplinas, 100, stdin);
+    printf("Digite seu curso: ");
+    fgets(curso,20,stdin);
     fflush(stdin);
+    printf("Digite qual seu periodo: ");
+    fflush(stdin);
+    scanf("%d", &periodo);
 
-    printf("\n\n\nEntao certo. Consegui captar as seguintes informacoes: Seu nome e %s e voce tem %s anos de idade sua matricula e %s. Voce mora no endereco %s. Que legal seu curso e %s voce esta no %s e essas sao suas seguintes diciplinas: %s \n\n\n", nome, idade, matricula, endereco, curso, periodo, diciplinas);
+    fflush(stdin);
+    printf("Digite suas disciplinas: ");
+    fgets(disciplinas,60,stdin);
+
+    printf("______________\n");
+    printf("Dados do Aluno\n");
+    printf("______________\n\n");
+
+    printf("Voce se chama %s" ,nome);
+    printf("Sua idade e %i\n" ,idade);
+    printf("Sua matricula %s" ,matricula);
+    printf("Seu endereco e %s" ,endereco );
+    printf("O curso que voce esta fazendo %s\n" ,curso);
+    printf("Esta no periodo %d" ,periodo);
+    printf("Suas diciplinas sao %s" ,disciplinas);
+
+    return 0;
 
 }
