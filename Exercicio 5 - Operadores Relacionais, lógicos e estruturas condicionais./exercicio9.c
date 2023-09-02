b338a244c0d0cc800013aa1b0ccdf950324125d2
ArhurBPA <arthurbpa2005@gmail.com>
2023-09-02 10:46:51 -0300

    02/09

--- Exercicio 5 - Operadores Relacionais, lógicos e estruturas condicionais./exercicio9.c
+++ Exercicio 5 - Operadores Relacionais, lógicos e estruturas condicionais./exercicio9.c
@@ -11,14 +11,33 @@ int main(){
     printf("Digite o tempo de colaboracao do funcionario:");
     scanf("%i", anosDeColaboracao);
 
-    if (salarioAtual < 500 && ){
+    if (salarioAtual <= 500 && anosDeColaboracao < 1){
         salarioComReajuste = (salarioAtual * (125 / 100));
-        printf("O salario com o reajuste e:%f", salarioComReajuste);
+        printf("O salario com o reajuste e:%.2f", salarioComReajuste);
     }
-    if ()
-    {
-        /* code */
+    if (salarioAtual > 500 && salarioAtual <= 1000 && anosDeColaboracao <= 3 && anosDeColaboracao >= 1){
+        salarioComReajuste = (salarioAtual * (120 / 100)+ 100);
+        printf("O salario com o reajuste e:%.2f", salarioComReajuste);
     }
+    if (salarioAtual > 1000 && salarioAtual <=1500 && anosDeColaboracao <= 6 && anosDeColaboracao >= 4){
+        salarioComReajuste = (salarioAtual * (115/100)+ 200);
+        printf("O salario com o reajuste e:%.2f", salarioComReajuste);
+    }
+    if (salarioAtual > 1500 && salarioAtual <= 2000 && anosDeColaboracao <= 10 && anosDeColaboracao >= 7){
+        salarioComReajuste = (salarioAtual * (110/100)+ 300);
+        printf("O salario com o reajuste e:%.2f", salarioComReajuste);
+    }
+    if (salarioAtual > 2000 && anosDeColaboracao > 10){
+        salarioComReajuste = salarioAtual = 500;
+        printf("O salario somente com o bonus e:%.2f", salarioComReajuste);
+    }
+
+
+
+
+    
+    
+    
     
     
 }
