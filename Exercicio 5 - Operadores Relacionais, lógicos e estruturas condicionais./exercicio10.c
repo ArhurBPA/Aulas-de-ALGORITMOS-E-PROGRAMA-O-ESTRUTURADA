11335abc3b8306ca2f1ee38ccc507f2539412e1e
ArhurBPA <arthurbpa2005@gmail.com>
2023-09-03 13:40:09 -0300

    summary

---
+++ Exercicio 5 - Operadores Relacionais, lógicos e estruturas condicionais./exercicio10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int delta, a, b, c;
+
+    printf("Entre com o valor de a:");
+    scanf("%d", a);
+    printf("Entre com o valor de b:");
+    scanf("%d", b);
+    printf("Entre com o valor de c:");
+    scanf("%d", c);
+
+    
+}
