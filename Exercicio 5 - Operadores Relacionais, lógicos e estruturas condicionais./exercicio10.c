9c10b1daf264fec02639c2f0015f4ba6b6df86f2
ArhurBPA <arthurbpa2005@gmail.com>
2023-09-03 21:13:53 -0300

    pronto

--- Exercicio 5 - Operadores Relacionais, lógicos e estruturas condicionais./exercicio10.c
+++ Exercicio 5 - Operadores Relacionais, lógicos e estruturas condicionais./exercicio10.c
@@ -5,13 +5,13 @@ int main(){
     int  a, b, c;
 
     float delta,resultado, resultadoNegativo, resultadoPositivo;
-    
+
     printf("Entre com o valor de a:");
-    scanf("%d", a);
+    scanf("%d", &a);
     printf("Entre com o valor de b:");
-    scanf("%d", b);
+    scanf("%d", &b);
     printf("Entre com o valor de c:");
-    scanf("%d", c);
+    scanf("%d", &c);
 
     delta = (b * b) - 4*a*c;
 
