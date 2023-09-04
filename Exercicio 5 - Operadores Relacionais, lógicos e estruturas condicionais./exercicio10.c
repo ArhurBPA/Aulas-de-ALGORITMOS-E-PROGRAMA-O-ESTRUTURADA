72330a0af6ca2f69368b8f89204c07a06f63df00
ArhurBPA <arthurbpa2005@gmail.com>
2023-09-03 21:12:27 -0300

    Update exercicio10.c

--- Exercicio 5 - Operadores Relacionais, lógicos e estruturas condicionais./exercicio10.c
+++ Exercicio 5 - Operadores Relacionais, lógicos e estruturas condicionais./exercicio10.c
@@ -1,14 +1,35 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-    int delta, a, b, c;
-
-    printf("Entre com o valor de a:");
+    int  a = 3, b = 6, c = 3;
+    float delta,resultado, resultadoNegativo, resultadoPositivo;
+    /*printf("Entre com o valor de a:");
     scanf("%d", a);
     printf("Entre com o valor de b:");
     scanf("%d", b);
     printf("Entre com o valor de c:");
-    scanf("%d", c);
+    scanf("%d", c);*/
 
+    delta = (b * b) - 4*a*c;
+
+    if (delta<0){
+
+        printf("Não existe raiz");
+
+    }else if(delta == 0){
+
+        resultado = (-b+(sqrt(delta)))/(2*a);
+        printf("Raiz  unica e %.0f", resultado);
+
+    }else if (delta > 0){
+
+        resultadoPositivo = (-b+(sqrt(delta)))/(2*a);
+        resultadoNegativo = (-b-(sqrt(delta)))/(2*a);
+
+        printf("A raiz negativa e %f e a positiva e %f", resultadoNegativo, resultadoPositivo);
+
+    }
+    
     
 }
