22d05301b265a55395e3a9f9b491f5df709a89b7
ArhurBPA <arthurbpa2005@gmail.com>
2023-09-23 09:35:39 -0300

    commit

--- 1Atividade Avaliativa/ex4.c
+++ 1Atividade Avaliativa/ex4.c
@@ -1,9 +1,10 @@
 /*Dados três valores A, B e C, construa um algoritmo para verificar se estes valores podem ser valores dos lados de um triângulo, e se for classificá-los (imprimi-los) segundo os ângulos. (Triângulo Retângulo = 90º, Triângulo Obtusângulo > 90º, Triângulo Acutângulo < 90º)*/
 #include <stdio.h>
+#include <math.h>
 int main()
 {
     int A, B, C;
-
+    double cosseno, anguloDotrianguloemradianos, anguloFinal;
     printf("Entre com um valor para A:");
     scanf("%d", &A);
 
@@ -13,9 +14,26 @@ int main()
     printf("Entre com um valor para C:");
     scanf("%d", &C);
 
+    cosseno = (A * A + B * B - C * C) / (2 * A * B);
+
+    anguloDotrianguloemradianos = acos(cosseno);
+
+    anguloFinal = anguloDotrianguloemradianos * 180.0 / M_PI;
+
     if (A + B > C && B + C > A && A + C > B)
     {
-       
+        if (anguloFinal == 90)
+        {
+            printf("É um triangulo Retângulo!");
+        }
+        if (anguloFinal > 90)
+        {
+            printf("É um triangulo Obtusângulo!");
+        }
+        if (anguloFinal < 90)
+        {
+            printf("É um triangulo Acutângulo!");
+        }
     }
     else
     {
